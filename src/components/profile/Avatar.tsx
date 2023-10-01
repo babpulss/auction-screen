21b7dd5895975ca56d952887fb1c74debc0e182d
babpulss <babpulss@naver.com>
2023-10-01 16:47:26 +0900

    feat - glow 효과  추가

--- src/components/profile/Avatar.tsx
+++ src/components/profile/Avatar.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import style from "./Avatar.module.css";
+import { useState } from "react";
 export default function Avatar() {
+  const [state, setState] = useState(false);
+
+  const onClickHandler = () => {
+    setState((prev) => !prev);
+  };
+
   return (
     <div>
       <Image
-        className="rounded-full"
+        onClick={onClickHandler}
+        className={`"rounded-full" ${state ? style.avatar : ""}`}
         src="/avatar.jpg"
         width="90"
         height="90"
